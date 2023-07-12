9f6c09e2011b60752d6ee365715b3fd27000f1f5
aivanov <alex130896@gmail.com>
2023-07-13 00:11:47 +0400

    cell refactoring

--- iOS10-HW13-Alexandr-Ivanov/Sources/MainScreen/Cells/DescribedTableViewCell.swift
+++ iOS10-HW13-Alexandr-Ivanov/Sources/MainScreen/Cells/DescribedTableViewCell.swift
@@ -1,13 +1,15 @@
 //
-//  DescriptionTableViewCell.swift
+//  DescribedTableViewCell.swift
 //  iOS10-HW13-Alexandr-Ivanov
 //
-//  Created by Александр Иванов on 24.06.2023.
+//  Created by Александр Иванов on 12.07.2023.
 //
 
 import UIKit
 
 class DescribedTableViewCell: GeneralTableViewCell {
+    override class var identifier: String { "described" }
+    
     override var option: Option? {
         didSet {
             switch option?.optionType {
@@ -18,9 +20,9 @@ class DescribedTableViewCell: GeneralTableViewCell {
             }
         }
     }
-
+    
     // MARK: - Outlets
-
+    
     private let describingLabel: UILabel = {
         let label = UILabel()
         label.text = "on"
@@ -29,25 +31,25 @@ class DescribedTableViewCell: GeneralTableViewCell {
         label.contentMode = .right
         return label
     }()
-
+    
     // MARK: - Initializers
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupHierarchy()
         setupLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     // MARK: - Setup
-
+    
     private func setupHierarchy() {
         addSubview(describingLabel)
     }
-
+    
     private func setupLayout() {
         describingLabel.snp.makeConstraints { make in
             make.centerY.equalTo(self)
